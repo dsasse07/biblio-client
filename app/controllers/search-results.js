b2f1a90870797917098697d8548a62a83f6d59b5
Danny Sasse <danny@travefy.com>
2021-10-11 16:06:17 -0400

    insert results placeholder

--- app/controllers/search-results.js
+++ app/controllers/search-results.js
@@ -4,3 +4,34 @@ import { inject as service } from '@ember/service';
 export default class SearchResultsController extends Controller {
   @service search;
 }
+
+/* 
+allowAnonLogging: true
+authors: ['Scott McNeely']
+averageRating: 5
+canonicalVolumeLink: "https://play.google.com/store/books/details?id=muqzayf-SpUC"
+categories: ['Games & Activities']
+contentVersion: "0.3.5.0.preview.3"
+description: "Classic and comprehensive, this guide to over 350 games is sure to appeal to all ages. From Bridge to Poker and Solitaireto Hearts, card games are a beloved source of entertainment and competition (and they are recession proof!). This authoritative book is ideal for every household, college dorm, family cabin, or neighborhood bar that has a pack of cards. Designed in the style of the popular Ultimate Bar Book, this essential resource provides the rules to dozensof variations of your favorite games, and a few you've probably never heard of (Bezique, anyone?). With simple instructions and clear illustrations to guide the way, this volume will be a welcome addition to any gamer's library."
+imageLinks:
+  smallThumbnail: "http://books.google.com/books/content?id=muqzayf-SpUC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+  thumbnail: "http://books.google.com/books/content?id=muqzayf-SpUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+[[Prototype]]: Object
+industryIdentifiers: (2) [{…}, {…}]
+infoLink: "https://play.google.com/store/books/details?id=muqzayf-SpUC&source=gbs_api"
+language: "en"
+maturityRating: "NOT_MATURE"
+pageCount: 400
+panelizationSummary:
+  containsEpubBubbles: false
+  containsImageBubbles: false
+[[Prototype]]: Object
+previewLink: "http://books.google.com/books?id=muqzayf-SpUC&printsec=frontcover&dq=card&hl=&cd=1&source=gbs_api"
+printType: "BOOK"
+publishedDate: "2012-01-06"
+publisher: "Chronicle Books"
+ratingsCount: 4
+readingModes: {text: true, image: true}
+subtitle: "The Comprehensive Guide to More than 350 Games"
+title: "Ultimate Book of Card Games"
+*/
